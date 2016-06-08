0059166708b331f72bb458be5d6e3a2971b9b9ee
root <marko.pascan@gmail.com>
2016-06-08 12:40:20 +0200

    Improvements. New Python class for deleting tables

--- create_db.py
+++ create_db.py
@@ -1,140 +1,161 @@
 import boto3
 
 dynamodb = boto3.resource('dynamodb', region_name='eu-central-1')
-
+dyndb_client = boto3.client('dynamodb', region_name='eu-central-1')
 tables = []
-tableName = 'ProductCatalog'
-print('Creating table: %s' % tableName)
 
-table = dynamodb.create_table(
-    TableName = tableName,
-    AttributeDefinitions = [
-        {
-            'AttributeName' : 'Id',
-            'AttributeType' : 'N'
+tableName = 'ProductCatalog'
+print('Checking if table %s exists.' % tableName)
+try:
+    description = dyndb_client.describe_table(TableName=tableName)
+    print('Table %s already exists. \n %s' % (tableName, description))
+except ResourceNotFoundException as ex:
+    print('Table %s does not exist. Creating table.' % tableName)
+    table = dynamodb.create_table(
+        TableName = tableName,
+        AttributeDefinitions = [
+            {
+                'AttributeName' : 'Id',
+                'AttributeType' : 'N'
+            }
+        ],
+        KeySchema = [
+            {
+                'AttributeName' : 'Id',
+                'KeyType' : 'HASH'
+            }
+        ],
+        ProvisionedThroughput = {
+            'ReadCapacityUnits' : 10,
+            'WriteCapacityUnits' : 5
         }
-    ],
-    KeySchema = [
-        {
-            'AttributeName' : 'Id',
-            'KeyType' : 'HASH'
-        }
-    ],
-    ProvisionedThroughput = {
-        'ReadCapacityUnits' : 10,
-        'WriteCapacityUnits' : 5
-    }
-)
-print('Creation of table %s was successful' % tableName)
-tables.append(tableName)
+    )
+    print('Creation of table %s was successful' % tableName)
+    tables.append(tableName)
 
 tableName = 'Forum'
-print('Creating table %s' % tableName)
-
-table = dynamodb.create_table(
-    TableName = tableName,
-    AttributeDefinitions = [
-        {
-            'AttributeName' : 'Name',
-            'AttributeType' : 'S'
+print('Checking if table %s exists.' % tableName)
+try:
+    description = dyndb_client.describe_table(TableName=tableName)
+    print('Table %s already exists. \n %s' % (tableName, description))
+except ResourceNotFoundException as ex:
+    print('Table %s does not exist. Creating table.' % tableName)
+    table = dynamodb.create_table(
+        TableName = tableName,
+        AttributeDefinitions = [
+            {
+                'AttributeName' : 'Name',
+                'AttributeType' : 'S'
+            }
+        ],
+        KeySchema = [
+            {
+                'AttributeName' : 'Name',
+                'KeyType' : 'HASH'
+            }
+        ],
+        ProvisionedThroughput = {
+            'ReadCapacityUnits' : 10,
+            'WriteCapacityUnits' : 5
         }
-    ],
-    KeySchema = [
-        {
-            'AttributeName' : 'Name',
-            'KeyType' : 'HASH'
-        }
-    ],
-    ProvisionedThroughput = {
-        'ReadCapacityUnits' : 10,
-        'WriteCapacityUnits' : 5
-    }
-)
-
-print('Creation of table %s was successful' % tableName)
-tables.append(tableName)
+    )
+    print('Creation of table %s was successful' % tableName)
+    tables.append(tableName)
 
 tableName = 'Thread'
-print('Creating table %s' % tableName)
+print('Checking if table %s exists.' % tableName)
+try:
+    description = dyndb_client.describe_table(TableName=tableName)
+    print('Table %s already exists. \n %s' % (tableName, description))
+except ResourceNotFoundException as ex:
+    print('Table %s does not exist. Creating table.' % tableName)
 
-table = dynamodb.create_table(
-    TableName = tableName,
-    AttributeDefinitions = [
-        {
-            'AttributeName' : 'ForumName',
-            'AttributeType' : 'S'
-        },
-        {
-            'AttributeName' : 'Subject',
-            'AttributeType' : 'S'
+    table = dynamodb.create_table(
+        TableName = tableName,
+        AttributeDefinitions = [
+            {
+                'AttributeName' : 'ForumName',
+                'AttributeType' : 'S'
+            },
+            {
+                'AttributeName' : 'Subject',
+                'AttributeType' : 'S'
+            }
+        ],
+        KeySchema = [
+            {
+                'AttributeName' : 'ForumName',
+                'KeyType' : 'HASH'
+            },
+            {
+                'AttributeName' : 'Subject',
+                'KeyType'       : 'RANGE'
+            }
+        ],
+        ProvisionedThroughput = {
+            'ReadCapacityUnits' : 10,
+            'WriteCapacityUnits' : 5
         }
-    ],
-    KeySchema = [
-        {
-            'AttributeName' : 'ForumName',
-            'KeyType' : 'HASH'
-        },
-        {
-            'AttributeName' : 'Subject',
-            'KeyType'       : 'RANGE'
-        }
-    ],
-    ProvisionedThroughput = {
-        'ReadCapacityUnits' : 10,
-        'WriteCapacityUnits' : 5
-    }
-)
-print('Creation of table %s was successful' % tableName)
-tables.append(tableName)
+    )
+    print('Creation of table %s was successful' % tableName)
+    tables.append(tableName)
 
 tableName = 'Reply'
-print('Creating table %s' % tableName)
-table = dynamodb.create_table(
-    TableName = tableName,
-    AttributeDefinitions = [
-        {
-            'AttributeName' : 'Id',
-            'AttributeType' : 'S'
-        },
-        {
-            'AttributeName' : 'ReplyDateTime',
-            'AttributeType' : 'S'
-        },
-        {
-            'AttributeName': 'PostedBy',
-            'AttributeType': 'S'
-        }
-    ],
-    LocalSecondaryIndexes = [
-        {
-            'IndexName' : 'PostedBy-index',
-            'KeySchema' : [
-                {
-                    'AttributeName' : 'Id',
-                    'KeyType'       : 'HASH'
-                },
-                {
-                    'AttributeName' : 'PostedBy',
-                    'KeyType' : 'RANGE'
-                }
-            ],
-            'Projection' : {
-                'ProjectionType' : 'KEYS_ONLY'
+print('Checking if table %s exists.' % tableName)
+try:
+    description = dyndb_client.describe_table(TableName=tableName)
+    print('Table %s already exists. \n %s' % (tableName, description))
+except ResourceNotFoundException as ex:
+
+    print('Table %s does not exist. Creating table.' % tableName)
+    table = dynamodb.create_table(
+        TableName = tableName,
+        AttributeDefinitions = [
+            {
+                'AttributeName' : 'Id',
+                'AttributeType' : 'S'
+            },
+            {
+                'AttributeName' : 'ReplyDateTime',
+                'AttributeType' : 'S'
+            },
+            {
+                'AttributeName': 'PostedBy',
+                'AttributeType': 'S'
             }
+        ],
+        LocalSecondaryIndexes = [
+            {
+                'IndexName' : 'PostedBy-index',
+                'KeySchema' : [
+                    {
+                        'AttributeName' : 'Id',
+                        'KeyType'       : 'HASH'
+                    },
+                    {
+                        'AttributeName' : 'PostedBy',
+                        'KeyType' : 'RANGE'
+                    }
+                ],
+                'Projection' : {
+                    'ProjectionType' : 'KEYS_ONLY'
+                }
+            }
+        ],
+        KeySchema = [
+            {
+                'AttributeName' : 'Id',
+                'KeyType' : 'HASH'
+            },
+            {
+                'AttributeName' : 'ReplyDateTime',
+                'KeyType' : 'RANGE'
+            }
+        ],
+        ProvisionedThroughput = {
+            'ReadCapacityUnits' : 10,
+            'WriteCapacityUnits' : 5
         }
-    ],
-    KeySchema = [
-        {
-            'AttributeName' : 'Id',
-            'KeyType' : 'HASH'
-        },
-        {
-            'AttributeName' : 'ReplyDateTime',
-            'KeyType' : 'RANGE'
-        }
-    ],
-    ProvisionedThroughput = {
-        'ReadCapacityUnits' : 10,
-        'WriteCapacityUnits' : 5
-    }
-)
+    )
+    print('Creation of table %s was successful' % tableName)
+    tables.append(tableName)
